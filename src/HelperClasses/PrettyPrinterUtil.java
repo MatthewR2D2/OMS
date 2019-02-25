1f3d1f4e684971165ff7576db76537c37be8e5b0
Matthew <matthew.c.r.millar@gmail.com>
2019-02-25 16:13:57 +0900

    Added java doc

--- src/HelperClasses/PrettyPrinterUtil.java
+++ src/HelperClasses/PrettyPrinterUtil.java
@@ -2,8 +2,15 @@ package HelperClasses;
 
 public class PrettyPrinterUtil {
 
+    /**+
+     * Constructor
+     */
     public PrettyPrinterUtil(){}
 
+    /**+
+     * Simple Printer helper
+     * @param stg
+     */
     public void print(String stg)
     {
         System.out.println(stg);
